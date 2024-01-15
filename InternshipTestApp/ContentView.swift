81740e4895d737764b53cef0c21ad80712126d3b
SF-testUmnyasha <newashedelo@gmail.com>
2024-01-15 16:17:30 +0200

    Initial Commit

---
+++ InternshipTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InternshipTestApp
+//
+//  Created by Oleg Arnaut  on 15.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InternshipTestApp/InternshipTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternshipTestAppApp.swift
+//  InternshipTestApp
+//
+//  Created by Oleg Arnaut  on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct InternshipTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
